29878bfa89c8e3b5084a292d99ab67af97180804
Alejandro Tos <altove@floridauniversiratia.es>
2023-01-13 20:18:06 +0100

    Añadiendo solucion 4 sh

--- solucion_final.sh
+++ solucion_final.sh
@@ -1,18 +1,25 @@
-#!bin/bash
+#!/bin/bash
 
+echo "-------------MENU D'OPCIONS:------------------"
+echo "Operació 1: BUSQUEDA DE NOMBRE DE CONNEXIONS PER NOM"
+echo "Operació 2: BUSQUEDA DE CONNEXIONS PER MES"
+echo "Operació 3: BUSQUEDA DE CONNEXIONS PER DIA I MES"
+echo "Operació 4: CONSULTA ÚLTIMA CONNEXIÓ DE L'USUARI"
+echo "Operació 5: EIXIDA DEL PROGRAMA"
+echo "----------------------------------------------"
 echo -n "Introduiex una opció: "
 read opc
 while [ $opc -ne 5 ]
 do
-case $opc
+case $opc in 
     1)
-        source soluciones1.sh ;;
+        source solucion1.sh ;;
     2)
-        source soluciones2.sh ;;
+        source solucion2.sh ;;
     3)
-        source soluciones3.sh ;;
+        source solucion3.sh ;;
     4)
-
+        source solucion4.sh ;;
     *)
         echo "Opció no valida." ;;
 esac
